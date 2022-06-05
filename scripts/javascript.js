a598438524f5c1a9bec59cc9628080d0c44039d1
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-05 01:11:50 -0400

    Create divs with JS only. Divs not yet in grid.

---
+++ scripts/javascript.js
@@ -0,0 +1,22 @@
+// Total number of squares per row or column (2-100)
+let numSquares = 6;
+
+
+// Create a div
+const parentDiv = document.createElement('div');
+parentDiv.classList.add("parentDiv");
+document.body.appendChild(parentDiv);
+
+parentDiv.appendChild(document.createElement('div'));
+parentDiv.appendChild(document.createElement('div'));
+parentDiv.appendChild(document.createElement('div'));
+parentDiv.appendChild(document.createElement('div'));
+
+
+
+
+
+// let myDiv1 = document.createElement('div');
+// myDiv1.classList.add("div-styles1");
+// parentDiv.appendChild(myDiv1);
+
