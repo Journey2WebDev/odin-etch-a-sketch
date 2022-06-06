8b4a2a87839010b4764b0bc37e17dfed3cc3c7c9
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-06 12:46:21 -0400

    Add create/clear grid functionality. Attach to 'create grid' button.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,39 +1,66 @@
-// n => number of squares per row or column ( 2 <= n <= 100 )
-let n = 10;
 
-// Create a div to contain all row/column divs (ie, all squares in grid)
-const bodyDiv = document.createElement('div');
-bodyDiv.classList.add("bodyDiv");
-document.body.appendChild(bodyDiv);
+// Update Grid Button
+let updateGridBtn = document.getElementById("updateGridBtn");
+updateGridBtn.addEventListener("click",getGridSize)
 
-for(row=1; row<=n; row++){
-  // Create n rows
-  const rowDiv = document.createElement('div');
-  rowDiv.classList.add("rowDiv");
-  rowDiv.setAttribute("id", "row-" + row);
-  bodyDiv.appendChild(rowDiv);
-
-  // Within each row, create n columns
-  for(col=1; col<=n; col++){
-    const normalDiv = document.createElement('div');
-    document.getElementById("row-" + row).appendChild(normalDiv);
-  }
-}
-
-// Toggle drawing mode (boolean; currently SHIFT)
-let tempVar = false;
+// Toggle drawing mode (boolean; currently SHIFT (keyCode 16))
+let allowDrawing = true;
 window.addEventListener("keydown", checkKeyPressed, false);
 
 function checkKeyPressed(evt) {
   if (evt.keyCode == "16") {
-    tempVar = !tempVar;
+    allowDrawing = !allowDrawing;
   }
 }
+
+// Create outline for grid (currently 90vh x 90vh)
+const gridDiv = document.createElement('div');
+gridDiv.classList.add("gridDiv");
+document.body.appendChild(gridDiv);
+
+// Clear the Grid
+function clearGrid(parent) {
+  while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+  }
+}
+
+// Prompt user for grid size
+function getGridSize(){
+  let squaresPerSide = parseInt(prompt("Enter number of squares per side (2 <= n <= 100)"));
+  // Check for valid input
+  if(isNaN(squaresPerSide) || (squaresPerSide < 2) || (squaresPerSide > 100)){
+    alert("Please enter a number between 2 and 100");
+    return;
+  } else {
+    // return Math.floor(squaresPerSide);
+    clearGrid(gridDiv);
+    createGrid(Math.floor(squaresPerSide));
+  }
+}
+
+function createGrid(squaresPerSide){
+  for(row=1; row<=squaresPerSide; row++){
+    // Create rows
+    const rowDiv = document.createElement('div');
+    rowDiv.classList.add("rowDiv");
+    rowDiv.setAttribute("id", "row-" + row);
+    gridDiv.appendChild(rowDiv);
+
+    // Within each row, create n columns
+    for(col=1; col<=squaresPerSide; col++){
+      const normalDiv = document.createElement('div');
+      document.getElementById("row-" + row).appendChild(normalDiv);
+    }
+  }
+}
+
+
 
 // [mouseenter] and [mouseleave] event for grid squares
 function beginDrawing(square){
   square.addEventListener("mouseenter", function(evt){
-    if(tempVar){
+    if(allowDrawing){
       evt.target.style.backgroundColor="orange";
     };
   });
@@ -44,23 +71,10 @@ function beginDrawing(square){
 }
 
 // Iterate over grid to add [mouseenter] and [mouseleave] events
-for(row=1; row<=n; row++){
-  let temp = document.getElementById("row-" + row).childNodes;
-  temp.forEach(beginDrawing);
-}
-
-
-
-
-// Create header at top
-// const headerDiv = document.createElement('div');
-// headerDiv.classList.add("headerDiv");
-// document.body.appendChild(headerDiv);
-
-// let headerBtn = document.createElement('button');
-// headerBtn.classList.add("headerBtn");
-// headerDiv.appendChild(headerBtn);
-
+// for(row=1; row<=n; row++){
+//   let temp = document.getElementById("row-" + row).childNodes;
+//   temp.forEach(beginDrawing);
+// }
 
 
 
