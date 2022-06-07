5eab9d2a50d980ae28093308dea47bcd1f88366a
Journey2WebDev <MaxwellHandy@gmail.com>
2022-06-07 13:06:10 -0400

    Add color wheel choice to header. Update grid size to 85 x 85 vh. Minor bug fixes.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,14 +1,28 @@
-
+// --------------------------------------------------
 // Create Grid Button
+// --------------------------------------------------
 let createGridBtn = document.getElementById("createGridBtn");
-createGridBtn.addEventListener("click",getGridSize);
+createGridBtn.addEventListener("click", getGridSize);
 
+// --------------------------------------------------
+// Button: Choose color
+// --------------------------------------------------
+let colorBox = document.getElementById("colorBox");
+currentColor = colorBox.value;
+
+colorBox.addEventListener("change", chooseColor);
+function chooseColor(){
+  currentColor = colorBox.value;
+}
+
+// --------------------------------------------------
 // Toggle: Gridlines (ie, border around grid squares)
+// --------------------------------------------------
 let gridlinesOn = false;
 
 function toggleGridlines(){
   let gridRows = document.getElementsByClassName('rowDiv')
-  
+
   for(r=0; r<gridRows.length; r++){
     let gridSquares = gridRows[r].childNodes;
 
@@ -27,7 +41,9 @@ function toggleGridlines(){
 let toggleGridBtn = document.getElementById("toggleGridlinesBtn");
 toggleGridBtn.addEventListener("click",toggleGridlines);
 
+// --------------------------------------------------
 // Button: Erase all
+// --------------------------------------------------
 function eraseAll(){
   let gridRows = document.getElementsByClassName('rowDiv')
   
@@ -42,8 +58,9 @@ function eraseAll(){
 let eraseAllBtn = document.getElementById("eraseAllBtn");
 eraseAllBtn.addEventListener("click",eraseAll);
 
-
+// --------------------------------------------------
 // Toggle: drawing mode (boolean; currently SHIFT (keyCode 16))
+// --------------------------------------------------
 let allowDrawing = true;
 window.addEventListener("keydown", checkKeyPressed, false);
 
@@ -53,13 +70,17 @@ function checkKeyPressed(evt) {
   }
 }
 
-// Create outline for grid (currently 90vh x 90vh)
+// --------------------------------------------------
+// Create outline for grid (currently 85vh x 85vh)
+// --------------------------------------------------
 const gridDiv = document.createElement('div');
 gridDiv.classList.add("gridDiv");
 document.body.appendChild(gridDiv);
 
-// Clear the Grid
-function clearGrid(parent) {
+// --------------------------------------------------
+// Remove Current Grid
+// --------------------------------------------------
+function removeGrid(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
   }
@@ -88,11 +109,13 @@ function createMouseEvents(squaresPerSide){
   }
 }
 
+// --------------------------------------------------
 // [mouseenter] and [mouseleave] event for grid squares
+// --------------------------------------------------
 function beginDrawing(square){
   square.addEventListener("mouseenter", function(evt){
     if(allowDrawing){
-      evt.target.style.backgroundColor="orange";
+      evt.target.style.backgroundColor=currentColor;
     };
   });
 
@@ -101,20 +124,32 @@ function beginDrawing(square){
   // });
 }
 
+// --------------------------------------------------
 // Prompt user for grid size
+// --------------------------------------------------
 function getGridSize(){
-  let squaresPerSide = parseInt(prompt("Enter number of squares per side (2 <= n <= 100)"));
+  // let squaresPerSide = parseInt(prompt("Enter number of squares per side (2 <= n <= 100)"));
+  let squaresPerSide = prompt("Enter number of squares per side (2 <= n <= 100)");
+
+  // Bug to fix: Update so 'cancel' doesn't prompt "Please enter a..." message
+  // console.log("Input box contents: " + squaresPerSide);
+  // console.log("Input box type: " + typeof squaresPerSide);
+
   // Check for valid input
   if(isNaN(squaresPerSide) || (squaresPerSide < 2) || (squaresPerSide > 100)){
     alert("Please enter a number between 2 and 100");
     return;
   } else {
     // Erase old grid; draw new grid; populate [mouseenter] and [mouseleave] events
-    clearGrid(gridDiv);
+    removeGrid(gridDiv);
     createGrid(Math.floor(squaresPerSide));
     createMouseEvents(squaresPerSide);
-    // Gridlines are off by default
+
+    // Reset Global vars
     gridlinesOn = false;
+    allowDrawing = true;
+    colorBox.value = "#000000";
+    currentColor = "#000000";
   }
 }
 
